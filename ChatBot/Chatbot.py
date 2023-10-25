065fe2618d6ad1418bdf7d8c66d17534fcf69ecc
Abhay Mathur <a21.mathur21@gmail.com>
2023-10-26 00:34:50 +0530

    added Chatbot

---
+++ ChatBot/Chatbot.py
@@ -0,0 +1,33 @@
+import openai
+import streamlit as st
+
+
+with st.sidebar:
+    
+    openai_api_key = "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"
+    
+
+    # "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    # "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("Recyclean-Waste Management Chatbot")
+st.caption("🚀 A Chatbot Powered by OpenAI LLM for overall education on waste management")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
