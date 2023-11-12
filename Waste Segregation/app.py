a1705e88cdc979240abc46aebe78c8fd2adc0c9c
Abhay Mathur <a21.mathur21@gmail.com>
2023-11-12 16:44:53 +0530

    Fully integrated with live model feed on webpage

--- Waste Segregation/app.py
+++ Waste Segregation/app.py
@@ -1,90 +1,78 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import cv2
+import base64
+import asyncio
 from ultralytics import YOLO
 import supervision as sv
-import base64
-import eventlet
-import numpy as np
 import time
 
-app = Flask(__name__)
-socketio = SocketIO(app)
+app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+model = YOLO(r'C:\Users\a21ma\OneDrive\Desktop\Code\Projects\IITR Sociothon\IITR_Sociothon\models\WasteSegregationSmall.pt')
 box_annotator = None
-is_processing = False
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+cap = cv2.VideoCapture(0)
 
-@socketio.on('connect')
-def handle_connect():
-    print("Connected")
-    model = YOLO(r"C:\Users\a21ma\OneDrive\Desktop\Code\Projects\IITR Sociothon\IITR_Sociothon\models\WasteSegregationSmall.pt")  
-    box_annotator = sv.BoxAnnotator(
-        thickness=2,
-        text_thickness=2,
-        text_scale=1
-    )
 
-    def send_processed_frame():
-        cap = cv2.VideoCapture(0)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+async def read_camera(websocket: WebSocket):
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Process the frame
+        box_annotator = sv.BoxAnnotator(
+            thickness=2,
+            text_thickness=2,
+            text_scale=1
+        )
         
         excluded_classes = [25,24,23]
         
-        while True:
-            start_time = time.perf_counter()
-            
-            ret, frame = cap.read()
-            
-            if not ret: break
-            
-            result = model(frame, agnostic_nms=True)[0]
-            
-            detections = sv.Detections.from_yolov8(result)
-            
-            filtered_detections = [
-                detection for detection in detections
-                if detection[1] >=0.2 and detection[2] not in excluded_classes
-            ]
-            
-            labels = [
-                f"{model.model.names[class_id]} {confidence:0.2f}" for _, confidence, class_id, _ in filtered_detections
-            ]
-            
-            frame = box_annotator.annotate(scene=frame, detections=filtered_detections, labels=labels)
-
-            _, encoded_frame = cv2.imencode('.jpg', frame)
-            frame_bytes = encoded_frame.tobytes()
-            frame_base64 = base64.b64encode(frame_bytes).decode()
-            socketio.emit('frame', frame_base64)
-
-            end_time = time.perf_counter()
-            fps = 1 / (end_time - start_time)
-            
-            print(f"FPS: {fps:.2f}")
-            cv2.putText(frame, f"FPS: {fps:.2f}",(20,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
-            
-            cv2.imshow("yolov8",frame)
-            
-            if cv2.waitKey(1) == 27 or cv2.getWindowProperty("yolov8", cv2.WND_PROP_VISIBLE) < 1:
-                break
+        start_time = time.perf_counter()
         
-    eventlet.spawn(send_processed_frame)
+        result = model(frame, agnostic_nms=True)[0]
+            
+        detections = sv.Detections.from_yolov8(result)
+        
+        filtered_detections = [
+            detection for detection in detections
+            if detection[1] >=0.2 and detection[2] not in excluded_classes
+        ]
+        
+        labels = [
+            f"{model.model.names[class_id]} {confidence:0.2f}" for _, confidence, class_id, _ in filtered_detections
+        ]
+        
+        frame = box_annotator.annotate(scene=frame, detections=filtered_detections, labels=labels)
+        
+        # Convert the frame to base64
+        _, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_text = base64.b64encode(buffer).decode('utf-8')
 
-@socketio.on('start_processing')
-def start_processing():
-    global is_processing
-    is_processing = True
+        # Send the frame to the WebSocket
+        await websocket.send_text(jpg_as_text)
+        
+        end_time = time.perf_counter()
+        fps = 1 / (end_time - start_time)
+        print(f"FPS: {fps:.2f}")
+        cv2.putText(frame, f"FPS: {fps:.2f}",(20,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            
+        # Introduce a delay (simulating real-time)
+        await asyncio.sleep(0.1)
 
-# Define a Socket.IO event to stop processing
-@socketio.on('stop_processing')
-def stop_processing():
-    global is_processing
-    is_processing = False
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    await read_camera(websocket)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
